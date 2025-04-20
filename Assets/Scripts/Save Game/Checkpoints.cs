31afcf9d360368cecff8f16e3dd614fcb38ff85a
MNedkov09 <martin.nedkov2009@gmail.com>
2025-04-20 21:03:37 +0300

    Checkpoints added

---
+++ Assets/Scripts/Save Game/Checkpoints.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class CheckpointData
+{
+    public int checkpointId;
+}
+
+public class Checkpoints : MonoBehaviour
+{
+    public static Checkpoints Instance;
+    public Transform[] checkpointPositions;
+
+    private string savePath;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        savePath = Path.Combine(Application.persistentDataPath, "checkpoint.json");
+    }
+
+    void Start()
+    {
+        LoadCheckpoint();
+    }
+
+    public void SaveCheckpoint(int id)
+    {
+        CheckpointData data = new CheckpointData { checkpointId = id };
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(savePath, json);
+    }
+
+    public void LoadCheckpoint()
+    {
+        if (!File.Exists(savePath))
+            return;
+
+        string json = File.ReadAllText(savePath);
+        CheckpointData data = JsonUtility.FromJson<CheckpointData>(json);
+
+        if (data.checkpointId >= 0 && data.checkpointId < checkpointPositions.Length)
+        {
+            Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+            player.position = checkpointPositions[data.checkpointId].position;
+        }
+    }
+}
